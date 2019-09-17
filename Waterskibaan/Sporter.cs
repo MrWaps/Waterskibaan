459253f7ba5197488539713990a49d4b1b6713a8
MrWaps <olafwapsenkamp@gmail.com>
2019-09-17 11:53:49 +0200

    Opdracht 1

---
+++ Waterskibaan/Sporter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    class Sporter
+    {
+        public int AantalRondenNogTeGaan { get
+            {
+                return 0;
+            }
+            set
+            {
+                
+            }
+        }
+
+        public Zwemvest Zwemvest { get; set; }
+        public Skies Skies { get; set; }
+        public Color KledingKleur { get; set; }
+        public List<IMoves> Moves { get; set; }
+
+        public Sporter(List<IMoves> moves)
+        {
+
+        }
+    }
+}
